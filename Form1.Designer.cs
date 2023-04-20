7bfed62b4e27bc9296d2f706e088edca47955e81
Danil Privalov <privatnik15@gmail.com>
2023-04-20 21:32:42 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PR2Danil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(278, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(243, 161);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Отсортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR2Danil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Monster monstr1 = new Monster(10,10);
+            Monster monstr2 = new Monster(20,20);
+            Monster monstr3 = new Monster(30,30);
+            Monster monstr4 = new Monster(40,40);
+            Monster monstr5 = new Monster(50,50);
+            Monster monstr6 = new Monster(60,60);
+            Monster monstr7 = new Monster(70,70);
+            Monster monstr8 = new Monster(80,80);
+            Monster monstr9 = new Monster(90,90);
+            Monster monstr10 = new Monster(100,100);
+
+            List<String> healthes = new List<String>();
+
+            healthes.Add(monstr1.health.ToString());
+            healthes.Add(monstr2.health.ToString());
+            healthes.Add(monstr3.health.ToString());
+            healthes.Add(monstr4.health.ToString());
+            healthes.Add(monstr5.health.ToString());
+            healthes.Add(monstr6.health.ToString());
+            healthes.Add(monstr7.health.ToString());
+            healthes.Add(monstr8.health.ToString());
+            healthes.Add(monstr9.health.ToString());
+            healthes.Add(monstr10.health.ToString());
+
+            listView1.Items.Add(monstr1.health.ToString());
+            listView1.Items.Add(monstr2.health.ToString());
+            listView1.Items.Add(monstr3.health.ToString());
+            listView1.Items.Add(monstr4.health.ToString());
+            listView1.Items.Add(monstr5.health.ToString());
+            listView1.Items.Add(monstr6.health.ToString());
+            listView1.Items.Add(monstr7.health.ToString());
+            listView1.Items.Add(monstr8.health.ToString());
+            listView1.Items.Add(monstr9.health.ToString());
+            listView1.Items.Add(monstr10.health.ToString());
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            listView1.Items.Add("70");
+            listView1.Items.Add("80");
+            listView1.Items.Add("90");
+            listView1.Items.Add("100");
+        }
+    }
+
+    class Monster
+    {
+        private int _health;
+        private int _power;
+        public int health
+        {
+            get { return _health; }
+            set
+            {
+                if (value > 100)
+                {
+                    _health = 100;
+                }
+
+                else if (value < 1)
+                {
+                    _health = 1;
+                }
+
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
+
+        public int power
+        {
+            get { return _power; }
+            set
+            {
+                if (value > 100)
+                {
+                    _power = 100;
+                }
+
+                else if (value < 1)
+                {
+                    _power = 1;
+                }
+
+                else
+                {
+                    _power = value;
+                }
+            }
+        }
+
+        public Monster(int hp, int pw)
+        {
+            health = hp;
+            power = pw;
+        }
+
+    }
+}
